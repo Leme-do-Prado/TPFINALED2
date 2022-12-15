411f65a6b6a46efdecedf2e4323094a7c3a10d58
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-12-14 21:00:53 -0300

    Update TipoProduto.cs

--- TipoProduto.cs
+++ TipoProduto.cs
@@ -1,4 +1,23 @@
+using System.Collections.Generic;  
+
 public TipoProduto{
-    private int idTipo { get; set; }
-    private int vlDiarioTipo { get; set; }
+    public int idTipo;
+    public int vlDiarioTipo;
+    public Queue<Produto> produtos;
+
+    public TipoProduto(int idTipo, int vlDiarioTipo){
+        this.idTipo = idTipo;
+        this.vlDiarioTipo = vlDiarioTipo;
+    }
+
+    public void enfileiraProduto(Produto produto, Queue<Produto> qtProdutosTipoProduto){
+        qtProdutosTipoProduto.enqueue(produto);
+    }
+
+    public Produto procuraProduto(int idp){
+    for (int i=0; i< this.ProdutosTipoProduto.Count; i++) {
+    if (ProdutosTipoProduto[i].id == idp) {
+        return produto[i];
+    break;
+    }
 }
