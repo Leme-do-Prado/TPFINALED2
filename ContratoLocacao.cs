f1c1130f7f4da7563288ca8d0e56f3cd85ad8c23
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-12-14 21:02:15 -0300

    Update ContratoLocacao.cs

--- ContratoLocacao.cs
+++ ContratoLocacao.cs
@@ -1,7 +1,13 @@
 public ContratoLocacao{
-    private DateTime retorno, saida;
+    public int idCliente, qt;
+    public TipoProduto tipo;
+    public DateTime retorno, saida;
+    public Queue<Produto> produtos;
 
-    public ContratoLocacao(){
+    public ContratoLocacao(TipoProduto tipo, int idCliente, int qt){
+        this.tipo = tipo;
+        this.idCliente = idCliente;
+        this.qt = qt;
         this.saida = DateTime.Now;
         this.retorno = new DateTime(DateTime.Now.Day+7, DateTime.Month, DateTime.Year);
     }
